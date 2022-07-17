00f1b8eaf7719ed3872e41fe468549749c96b95a
James Taylor <jata20@student.bth.se>
2022-07-17 14:02:39 +0100

    Finished the sidebar and added navigationbar

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,28 @@
+import React,{createContext, Profiler, useContext, useState} from "react";
+
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfiler: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true)
+    
+    return (
+        <StateContext.Provider
+            value={{ 
+                activeMenu,
+                setActiveMenu
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
